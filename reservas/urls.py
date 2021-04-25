6e46a6fe10346e1469ebca96c341e85fab09e041
Stefan Braitti <sbraitti@gmail.com>
2021-04-24 23:05:47 -0300

    Template Login

--- reservas/urls.py
+++ reservas/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
+from reservas import views
 
 urlpatterns = [
-    path('teste/admin/', admin.site.urls),
+    path('', views.login, name='login'),
 ]
