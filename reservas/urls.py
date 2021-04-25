960d2d065d5d4d14addb062687930a72d3b82e39
Stefan Braitti <sbraitti@gmail.com>
2021-04-25 11:55:23 -0300

    Calendar

--- reservas/urls.py
+++ reservas/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 from reservas import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('', views.lotus_login, name='lotus_login'),
     path('logout/', views.lotus_logout, name='lotus_logout'),
+    url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'), # here
 ]
--- reservas/views.py
+++ reservas/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import requires_csrf_token
-
+from datetime import datetime
+from django.http import HttpResponse
+from django.views import generic
+from django.utils.safestring import mark_safe
+from .models import *
+from .utils import Calendar
 
 def lotus_login(request):
     if request.user.is_authenticated:
@@ -28,4 +33,32 @@ def lotus_login(request):
 def lotus_logout(request):
     logout(request)
     context={}
-    return render(request, 'login.html', context=context) 
+    return render(request, 'login.html', context=context)
+
+
+#Calendario
+
+
+class CalendarView(generic.ListView):
+    model = Event
+    template_name = 'calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # use today's date for the calendar
+        d = get_date(self.request.GET.get('day', None))
+
+        # Instantiate our calendar class with today's year and date
+        cal = Calendar(d.year, d.month)
+
+        # Call the formatmonth method, which returns our calendar as a table
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        return context
+
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
